1ad5626dffbb6b650677bff84f318a514a31e019
ilkhomjonmavlonov-create <ilkhomjonmavlonov@gmail.com>
2025-10-04 14:30:00 -0400

    Update hw.sql

--- lesson-2/homework/hw.sql
+++ lesson-2/homework/hw.sql
@@ -44,3 +44,76 @@ select* from Departments
 
 
 truncate table Employees
+
+----
+alter table Employees
+alter column Name varchar(100)
+
+
+alter table Employees
+add Department VARCHAR(50)
+
+SELECT* from Employees
+
+alter table Employees
+alter column Salary float
+
+truncate table Employees
+
+UPDATE Employees
+set Department = 'IT'
+where EmpID = 1
+
+INSERT INTO Employees values (2, 'Komil', 2500, 'Logistics')
+INSERT INTO Employees values (4, 'Sarvar', 2700, 'Dispatcher')
+INSERT INTO Employees values (5, 'Komil', 6500, 'Manager')
+
+
+
+
+SELECT *
+INTO Departments
+FROM Employees
+
+SELECT* from Departments
+
+UPDATE Departments
+set Department = 'Management'
+where Salary > 5000
+
+
+truncate table Departments
+
+alter table Employees
+drop column Department
+
+SELECT* from Employees
+
+--3
+create database sql_learning
+go
+use sql_learning
+
+create table Products (id int, name varchar(50), price decimal(10,2))
+
+select* from Products
+
+insert into Products values  (1, 'Apple', 0.99),
+                             (2, 'Banana', 0.59),
+                             (3, 'Orange', 1.29),
+                             (4, 'Milk', 2.49),
+                             (5, 'Bread', 1.99)
+
+SELECT * 
+INTO Products_Backup
+FROM Products
+
+EXEC sp_rename 'Products', 'Inventory'
+
+ALTER TABLE Inventory
+ALTER COLUMN Price FLOAT
+
+alter table Inventory
+add Productcode int Identity(1000,5)
+
+select * from Inventory
